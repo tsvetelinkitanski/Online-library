525b577bb83e19848407f92bc6621133c07956fb
Tsvetelin Kitanski <106109735+tsvetelinkitanski@users.noreply.github.com>
2023-02-22 09:15:12 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,53 +1,55 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom'
-import { AuthContext } from '../src/authContext/AuthContext'
-import useLocalStorage from './hooks/useLocalStorage';
-import Header from './components/Header/Header.js'
-import Dashboard from './components/Dashboard/Dashboard.js'
-import Login from './components/Login/Login.js'
-import Register from './components/Register/Register.js'
-import Details from './components/Details/Details.js'
-import Create from './components/Create/Create.js'
-import Edit from './components/Edit/Edit.js'
-import MyBooksPage from './components/MyBooksPage/MyBooksPage.js'
-import Logout from './components/Logout/Logout';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { AuthContext } from "../src/authContext/AuthContext";
+import { logout } from "./api/user";
+import useLocalStorage from "./hooks/useLocalStorage";
+import Header from "./components/Header/Header.js";
+import Dashboard from "./components/Dashboard/Dashboard.js";
+import Login from "./components/Login/Login.js";
+import Register from "./components/Register/Register.js";
+import Details from "./components/Details/Details.js";
+import Create from "./components/Create/Create.js";
+import Edit from "./components/Edit/Edit.js";
+import MyBooksPage from "./components/MyBooksPage/MyBooksPage.js";
+import Logout from "./components/Logout/Logout";
 
 function App() {
-  const [user, setUser] = useLocalStorage('user', {
-    _id: '',
-    email: '',
-    accessToken: '',
-  })
+  const [user, setUser] = useLocalStorage("user", {
+    _id: "",
+    email: "",
+    accessToken: "",
+  });
 
   const onLoginOrRegister = (authData) => {
-    setUser(authData)
-  }
+    setUser(authData);
+  };
 
   const logoutHandler = () => {
+    logout();
     setUser({
-      _id: '',
-      email: '',
-      accessToken: '',
-    })
-  }
+      _id: "",
+      email: "",
+      accessToken: "",
+    });
+  };
   return (
     <AuthContext.Provider value={{ onLoginOrRegister, logoutHandler }}>
-
       <div id="root">
         <Header email={user.email} />
         <main id="site-content">
-
           <Routes>
-            <Route path='/' element={<Dashboard />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/details/:id' element={<Details user={user} />} />
-            <Route path='/create' element={<Create user={user} />} />
-            <Route path='/edit/:id' element={<Edit user={user} />} />
-            <Route path='/my-books-page' element={<MyBooksPage user={user} />} />
-            <Route path='/logout' element={<Logout />} />
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/details/:id" element={<Details user={user} />} />
+            <Route path="/create" element={<Create user={user} />} />
+            <Route path="/edit/:id" element={<Edit user={user} />} />
+            <Route
+              path="/my-books-page"
+              element={<MyBooksPage user={user} />}
+            />
+            <Route path="/logout" element={<Logout />} />
           </Routes>
-
         </main>
 
         <footer id="site-footer">
--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,18 +1,14 @@
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { logout } from "../../api/user";
 import { AuthContext } from "../../authContext/AuthContext";
 
 const Logout = () => {
-  const { logoutHandler } = useContext(AuthContext);
+  const { logoutHandler, user } = useContext(AuthContext);
   const navigate = useNavigate();
 
   useEffect(() => {
-    logout().then(() => {
-      console.log("logout");
-      navigate("/");
-      logoutHandler();
-    });
+    navigate("/");
+    logoutHandler();
   }, []);
 };
 
